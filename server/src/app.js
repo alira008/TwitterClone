a5ae149c7de8feb08442fd1e92ed198fc85cffc0
alira008 <ariellira.al@gmail.com>
2021-08-23 15:17:35 -0700

    fixed bug so that cookies are automatically req

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandler);
