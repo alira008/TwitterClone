b0890332c3d61b421dc147b92ab1e5e0179d8fa2
alira008 <ariellira.al@gmail.com>
2021-09-03 10:13:22 -0700

    added temp avatars for users

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { AvatarWrapper } from './Avatar.styles';
 
 interface Props {
-    size?: string
-    link?: string
+	size?: string;
+	link?: string;
+	seed?: string;
 }
 
-const Avatar: React.FC<Props> = ({ size, link }) => {
-    return (
-        <AvatarWrapper size={size} href={link}>
-            <img src="" alt="pp" />
-        </AvatarWrapper>
-    )
+const Avatar: React.FC<Props> = ({ size, link, seed }) => {
+	const customPicture =
+		'https://avatars.dicebear.com/api/bottts/' +
+		seed +
+		'.svg?backgroundColor=%23007bff';
+	return (
+		<AvatarWrapper size={size} href={link}>
+			{seed ? <img src={customPicture} alt="pp" /> : <img src="" alt="pp" />}
+		</AvatarWrapper>
+	);
 };
 
 export default Avatar;
