bfd231bb453af9406f1a6dfbe0e42270dd9abc1c
alira008 <ariellira.al@gmail.com>
2021-09-03 10:15:26 -0700

    fixed routing issue + added register user service

--- client/src/modules/API/AuthApiProvider.ts
+++ client/src/modules/API/AuthApiProvider.ts
@@ -5,6 +5,13 @@ interface LoginData {
 	uid: string;
 }
 
+interface RegisterData {
+	username: string;
+	userHandle: string;
+	password: string;
+	dateOfBirth: string;
+}
+
 export const loginService = async (username: string, password: string) => {
 	const route = '/auth/login';
 	const userData = {
@@ -12,6 +19,25 @@ export const loginService = async (username: string, password: string) => {
 		password: password,
 	};
 	const response = await axios.post<LoginData>(route, userData);
+	const data = response.data;
+	return data;
+};
+
+export const registerService = async (
+	username: string,
+	userHandle: string,
+	password: string,
+	dateOfBirth: string
+) => {
+	const route = '/auth/register';
+	const userData = {
+		username: username,
+		userHandle: userHandle,
+		password: password,
+		dateOfBirth: dateOfBirth,
+	};
+
+	const response = await axios.post<RegisterData>(route, userData);
 	const data = response.data;
 	return data;
 };
--- client/src/modules/routing/Routing.tsx
+++ client/src/modules/routing/Routing.tsx
@@ -5,6 +5,7 @@ import Home from '../../pages/HomePage/Home';
 import Landing from '../../pages/LandingPage/Landing';
 import Login from '../../pages/LoginPage/Login';
 import Register from '../../pages/RegisterPage/Register';
+import UserProfile from '../../pages/UserProfilePage/UserProfile';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
@@ -13,11 +14,12 @@ interface Props {}
 const Routing: React.FC<Props> = () => {
 	return (
 		<Switch>
-			<PrivateRoute exact path="/home" component={Home} />
-			<PrivateRoute exact path="/compose/tweet" component={ComposeTweetPage} />
 			<PublicRoute exact path="/" component={Landing} />
 			<PublicRoute exact path="/login" component={Login} />
 			<PublicRoute exact path="/register" component={Register} />
+			<PrivateRoute exact path="/home" component={Home} />
+			<PrivateRoute exact path="/compose/tweet" component={ComposeTweetPage} />
+			<PrivateRoute path="/:username" component={UserProfile} />
 			<Redirect to="/" />
 		</Switch>
 	);
