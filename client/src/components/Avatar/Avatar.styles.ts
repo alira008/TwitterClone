c22bd035a31c4f5caced670904545546df24da2a
alira008 <ariellira.al@gmail.com>
2021-09-17 19:50:58 -0700

    added option to change to button or anchor tag

--- client/src/components/Avatar/Avatar.styles.ts
+++ client/src/components/Avatar/Avatar.styles.ts
@@ -1,32 +1,41 @@
 import styled from 'styled-components';
 
 interface Props {
-    size?: string;
+	size?: string;
 }
 
-export const AvatarWrapper = styled.a<Props>`
-    overflow: hidden;
-    background-color: white;
-    height: ${({ size }) =>
-        //  if size === "md"
-        size === "md" ? "46px" :
-            //  else if size === "lg"
-            size === "lg" ? "81px" :
-                //  else
-                "32px"
-    };
-    width: ${({ size }) =>
-        //  if size === "md"
-        size === "md" ? "46px" :
-            //  else if size === "lg"
-            size === "lg" ? "81px" :
-                //  else
-                "32px"
-    };
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: black;
-    text-decoration: none;
+export const ButtonWrapper = styled.div`
+	cursor: pointer;
+`;
+
+export const AnchorWrapper = styled.a`
+	text-decoration: none;
+`;
+
+export const AvatarWrapper = styled.div<Props>`
+	overflow: hidden;
+	background-color: white;
+	height: ${({ size }) =>
+		//  if size === "md"
+		size === 'md'
+			? '46px'
+			: //  else if size === "lg"
+			size === 'lg'
+			? '81px'
+			: //  else
+			  '32px'};
+	width: ${({ size }) =>
+		//  if size === "md"
+		size === 'md'
+			? '46px'
+			: //  else if size === "lg"
+			size === 'lg'
+			? '81px'
+			: //  else
+			  '32px'};
+	border-radius: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: black;
 `;
--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { AvatarWrapper } from './Avatar.styles';
+import { AvatarWrapper, ButtonWrapper, AnchorWrapper } from './Avatar.styles';
 
 interface Props {
 	size?: string;
 	link?: string;
 	seed?: string;
+	onClick?: () => void;
 }
 
-const Avatar: React.FC<Props> = ({ size, link, seed }) => {
+const Avatar: React.FC<Props> = ({ size, link, seed, onClick }) => {
 	const customPicture =
 		'https://avatars.dicebear.com/api/bottts/' +
 		seed +
 		'.svg?backgroundColor=%23007bff';
-	return (
-		<AvatarWrapper size={size} href={link}>
-			{seed ? <img src={customPicture} alt="pp" /> : <img src="" alt="pp" />}
-		</AvatarWrapper>
+
+	const avatarElement = seed ? (
+		<img src={customPicture} alt="pp" />
+	) : (
+		<img src="" alt="pp" />
+	);
+
+	return link ? (
+		<AnchorWrapper href={link}>
+			<AvatarWrapper size={size}>{avatarElement}</AvatarWrapper>
+		</AnchorWrapper>
+	) : (
+		<ButtonWrapper onClick={onClick}>
+			<AvatarWrapper size={size}>{avatarElement}</AvatarWrapper>
+		</ButtonWrapper>
 	);
 };
 
