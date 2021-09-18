3a3acffb6b083eb4987218880668044bd71bbdfd
alira008 <ariellira.al@gmail.com>
2021-09-17 19:45:34 -0700

    added a separator component

---
+++ client/src/components/Separator/Separator.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	margin: 2px 0;
+	height: 1px;
+	background-color: var(--border-color);
+`;
---
+++ client/src/components/Separator/Separator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Wrapper } from './Separator.styles';
+interface Props {}
+
+const Separator: React.FC<Props> = (props: Props) => {
+	return <Wrapper />;
+};
+
+export default Separator;
